0f355a621ed6df5da4af7c0f468dc116655ade14
Wayne Trout <troutwayne@gmail.com>
2025-02-04 01:10:25 -0600

    Clean up deleted files and restore .gitignore

--- document_extractor/document_processor/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DocumentProcessorConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'document_processor'
--- document_extractor/document_processor/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import upload_document
-
-urlpatterns = [
-    path('upload/', upload_document, name='upload_document'),
-]
